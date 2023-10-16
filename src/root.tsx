640a768c1ed751fa63088e6946e4a59e55ec0f6c
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-10-16 15:00:54 -0300

    chore: remove all noise code

--- src/root.tsx
+++ src/root.tsx
@@ -4,29 +4,54 @@ import {
   RouterOutlet,
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
+import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
 
-import "./global.css";
+import "~/styles/global.css";
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
-
   return (
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
-        <RouterHead />
+        <Head />
         <ServiceWorkerRegister />
       </head>
       <body lang="en">
         <RouterOutlet />
       </body>
     </QwikCityProvider>
+  );
+});
+
+
+export const Head = component$(() => {
+  const head = useDocumentHead();
+  const loc = useLocation();
+
+  return (
+    <>
+      <title>{head.title}</title>
+
+      <link rel="canonical" href={loc.url.href} />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+      {head.meta.map((m) => (
+        <meta key={m.key} {...m} />
+      ))}
+
+      {head.links.map((l) => (
+        <link key={l.key} {...l} />
+      ))}
+
+      {head.styles.map((s) => (
+        <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
+      ))}
+
+      {head.scripts.map((s) => (
+        <script key={s.key} {...s.props} dangerouslySetInnerHTML={s.script} />
+      ))}
+    </>
   );
 });
