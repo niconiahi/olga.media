bf9953c0d3bb7832a355881aef6b38b2dd300684
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-10-21 17:51:59 -0300

    feat: apply css

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -3,9 +3,7 @@ import { component$, Slot } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <>
-      <main>
-        <Slot />
-      </main>
+      <Slot />
     </>
   );
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,20 @@ module.exports = {
     extend: {
       colors: {
         brand: {
-          blue: "#016aac",
+          blue: "#015fa0",
           red: "#ff0044",
+        },
+        show: {
+          seriaIncreible: {
+            yellow: "#f8c552",
+            purple: "#6f49a0",
+            purpleHover: "hsl(266 37% 46% / 20%)",
+          },
+          soneQueVolaba: {
+            blue: "#006ba6",
+            blueHover: "hsl(201 100% 33% / 20%)",
+            orange: "#ff9600",
+          },
         },
       },
     },
