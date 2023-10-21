5d0e9605ba0aceec17463ba46d311c1b284acce3
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-10-21 20:27:25 -0300

    feat: add margins on mobile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -81,7 +81,7 @@ export default component$(() => {
   const list = useList();
 
   return (
-    <main class="mx-auto my-8 max-w-max space-y-2">
+    <main class="mx-auto my-8 max-w-max space-y-2 px-2 md:my-2">
       <ul class="space-y-2">
         {list.value
           .slice()
