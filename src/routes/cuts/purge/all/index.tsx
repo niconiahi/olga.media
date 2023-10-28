a5f9630fedfa733f82a3e9529b3b8876d665ade9
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-10-28 20:41:30 -0300

    feat: add mechanism to purge the list of cuts endpoint

---
+++ src/routes/cuts/purge/all/index.tsx
@@ -0,0 +1,32 @@
+import { z, type RequestHandler } from "@builder.io/qwik-city";
+
+export const cutsSchema = z.array(
+  z.object({
+    start: z.string(),
+    label: z.string(),
+    day: z.number(),
+    hash: z.string(),
+    month: z.number(),
+    show: z.string(),
+  }),
+);
+
+export type Cuts = z.infer<typeof cutsSchema>;
+
+export const onGet: RequestHandler = async ({ json }) => {
+  const CLOUDFLARE_PURGE_URL =
+    "https://api.cloudflare.com/client/v4/zones/identifier/purge_cache";
+  const OLGA_URL = "https://olga-tv.pages.dev";
+
+  const data = await (
+    await fetch(CLOUDFLARE_PURGE_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        prefixes: [`${OLGA_URL}/cuts/get/all`],
+      }),
+    })
+  ).json();
+
+  json(200, data.success);
+};
