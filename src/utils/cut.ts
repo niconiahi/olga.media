1335276b50d661d4cdd843c3fc8da0ad6e8971fc
Nicolas Accetta <nicolas.accetta@gmail.com>
2023-11-06 19:32:04 -0300

    feat(utils/cut): improve dedupe + format cut start by making sure they use two digit

--- src/utils/cut.ts
+++ src/utils/cut.ts
@@ -1,15 +1,33 @@
 import type { Cuts } from "~/routes/add";
 
+function getSafeStart(start: string) {
+  const pieces = start.replaceAll("\\n", "").split(":");
+
+  return pieces
+    .map((digit, i) => {
+      if (pieces.length === 3 && i === 0) {
+        return digit;
+      }
+
+      if (pieces.length === 2 && i === 0) {
+        return digit.padStart(2, "0");
+      }
+
+      return digit.padEnd(2, "0");
+    })
+    .join(":");
+}
+
 export function getRaws(html: string, videoId: number): unknown[] {
   const raws = [] as unknown[];
   const regex =
-    /(\\n\d{1,2}:\d{2}(?::\d{2})?)\s(.*?)(?=\\n\d{1,2}:\d{2}(?::\d{2})?|\\n\\n|$)/g;
+    /(\\n\d{1,2}:\d{2}(?::\d{1,2})?)\s(.*?)(?=\\n\d{1,2}:\d{2}(?::\d{1,2})?|\\n\\n|$)/g;
 
   let match;
   while ((match = regex.exec(html)) !== null) {
     const [, start, label] = match;
     raws.push({
-      start: start.replaceAll("\\n", ""),
+      start: getSafeStart(start),
       label: label.trim().replaceAll("\\", ""),
       videoId,
     });
@@ -19,10 +37,9 @@ export function getRaws(html: string, videoId: number): unknown[] {
 }
 
 export function dedupe(cuts: Cuts) {
-  const index = cuts.slice(1, cuts.length).findIndex((element) => {
-    const regex = /^0{1,2}:\d{1,2}$/;
-
-    return regex.test(element.start);
+  const { start } = cuts[0];
+  const index = cuts.slice(1, cuts.length).findIndex((cut) => {
+    return cut.start === start;
   });
 
   return cuts.slice(0, index + 1);
